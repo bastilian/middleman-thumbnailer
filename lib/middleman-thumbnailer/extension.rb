d0b9528abcb2767bba101ef5aa333e748db06972
Nicholas Hemsley <nick.hems@gmail.com>
2014-02-25 17:16:38 +0800

    put glob string into glob_str var

--- lib/middleman-thumbnailer/extension.rb
+++ lib/middleman-thumbnailer/extension.rb
@@ -75,7 +75,8 @@ module Middleman
     class DirGlob
       def self.glob(root, namespaces, filetypes)
         filetypes_with_capitals = filetypes.reduce([]) { |memo, file| memo.concat [file, file.upcase] }
-        Dir["#{root}/#{namespaces.join(',')}/**/*.{#{filetypes_with_capitals.join(',')}}"]
+        glob_str = "#{root}/#{namespaces.join(',')}/**/*.{#{filetypes_with_capitals.join(',')}}"
+        Dir[glob_str]
       end
     end
 
