04f844c12e4d605c16b54f19f7454df563e0e358
Sebastian Gräßl <sebastian@validcode.me>
2016-03-28 10:11:18 +0200

    Replace deprecated current_dir with expand_path(".")

--- features/step_definitions/server_steps.rb
+++ features/step_definitions/server_steps.rb
@@ -3,26 +3,26 @@ require 'RMagick'
 require 'middleman-thumbnailer/thumbnail-generator'
 
 Then(/^the image "(.*?)" should have width of "([0-9]*?)"$/) do |path, width|
-  full_path = File.join(current_dir, path)
+  full_path = File.join(expand_path("."), path)
   image = ::Magick::Image.read(full_path).first
   image.columns.should == width.to_i
 end
 
 Then(/^the image "(.*?)" should have height of "(.*?)"$/) do |path, height|
-  full_path = File.join(current_dir, path)
+  full_path = File.join(expand_path("."), path)
   image = ::Magick::Image.read(full_path).first
   image.rows.should == height.to_i
 end
 
 Then (/^I should be able to rebuild "(.*?)" and the thumbnails do not regenerate$/) do |path|
-  image_path = File.join(current_dir, 'build', path)
+  image_path = File.join(expand_path("."), 'build', path)
 
   specs =  {
     :small => '200x',
     :medium => 'x300'
   }
   thumbnail_paths = ::Middleman::ThumbnailGenerator.specs(image_path, specs)
-  thumbnail_path = File.join(Dir.pwd, thumbnail_paths[:small][:name])
+  thumbnail_path = thumbnail_paths[:small][:name]
   current_mtime = File.mtime(thumbnail_path)
   sleep 1
   step %Q{I run `middleman build`}
@@ -31,8 +31,8 @@ Then (/^I should be able to rebuild "(.*?)" and the thumbnails do not regenerate
 end
 
 Then (/^I should be able to update an image "(.*?)" and the thumbnails regenerate$/) do |path|
-  source_path = File.join(current_dir, 'source', path)
-  image_path = File.join(current_dir, 'build', path)
+  source_path = File.join(expand_path("."), 'source', path)
+  image_path = File.join(expand_path("."), 'build', path)
   specs =  {
     :small => '200x',
     :medium => 'x300'
@@ -43,7 +43,7 @@ Then (/^I should be able to update an image "(.*?)" and the thumbnails regenerat
   updated_mtime = Time.now
   File.utime(updated_mtime, updated_mtime, source_path)
   step %Q{I run `middleman build`}
-  thumbnail_path = File.join(Dir.pwd, thumbnail_paths[:small][:name])
+  thumbnail_path = thumbnail_paths[:small][:name]
   new_mtime = File.mtime(thumbnail_path)
   expect(new_mtime.sec).to be(updated_mtime.sec)
 end
