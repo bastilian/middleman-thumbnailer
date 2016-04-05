10c8d4477cd31e2f065eae41dcb1aceecfaa58a3
Sebastian Gräßl <sebastian@validcode.me>
2016-04-05 11:04:24 +0200

    Allow expectations on nil in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,9 @@ require 'middleman'
 require 'middleman-thumbnailer'
 
 require_relative 'support/fixture'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.allow_message_expectations_on_nil = true
+  end
+end
