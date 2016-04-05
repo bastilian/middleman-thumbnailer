75a649045729e252ee944a45b51564d020d80546
Sebastian Gräßl <sebastian@validcode.me>
2016-04-05 11:04:05 +0200

    Add Middleman::Fixture.app to mock application instances

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+require 'middleman'
 require 'middleman-thumbnailer'
+
+require_relative 'support/fixture'
